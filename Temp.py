a1759853143f373eef07ce851bbe315defebe9ba
thuzax <arthucruz@hotmail.com>
2018-09-27 01:49:12 -0300

    A partir de title e minimo de 10 comments gera um texto exemplo

--- Temp.py
+++ Temp.py
@@ -1,13 +1,20 @@
 import json
 import csv
-from RemoveInvalidos import removeInvalidos, geraDicionarioComentarios
+from TrataComentarios import removeInvalidos, geraDicionarioComentario, uneTituloComentarios
 
 def main(args):
-    titulo = args["titulo"]
-    comentarios = args["comentarios"]
-    comentariosVideos = geraDicionarioComentarios(titulo, comentarios)
+    titulo = args["title"]
+    comentarios = args["comments"]
+    comentariosVideos = {}
+    quantidadeAdicionadaVideo = {}
+    for comentario in comentarios:
+        comentariosVideos = geraDicionarioComentario([0, comentario], comentariosVideos, quantidadeAdicionadaVideo, 10)
+
+    texto = uneTituloComentarios([0, titulo], comentariosVideos, 0)
+    print(texto)
+    
 
 
 
 
-main()
+main({"title" : "I hate the americans", "comments" : ["TOP", "good, little boy", "ooh little guy!", "this one i already know that he could win on life", "suuuucks dilma"]})
