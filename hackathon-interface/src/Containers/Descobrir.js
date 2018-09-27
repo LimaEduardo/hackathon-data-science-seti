6dc94b90a6d5ba2d8b3e68c7f0ab6067d5130094
limaeduardo <eduardo.fernandodelima@gmail.com>
2018-09-27 07:46:56 -0300

    improve visuals

--- hackathon-interface/src/Containers/Descobrir.js
+++ hackathon-interface/src/Containers/Descobrir.js
@@ -13,23 +13,45 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
+  },
+  paperRoot: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  discoveryContainer: {
+    paddingTop: theme.spacing.unit * 6,
+  },
+  videoURLTF: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 5,
+  },
+  messageContainer: {
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 2,
   }
 });
 
 
 function RenderLoadingMessage(props){
+  const {classes} = props
   if (props.message !== ""){
     return (
-      <div>
-        <Typography variant="subheading" gutterBottom>
-          {props.message}
-        </Typography>
-        <CircularProgress/>
-      </div>
+      <Grid container className={classes.messageContainer} direction="row" justify="center" alignItems="center">
+        <Grid item xs={10}>
+          <Typography variant="subheading" gutterBottom align="center">
+            {props.message}
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <CircularProgress/>
+        </Grid>
+      </Grid>
     )
   } else {
     return null
@@ -37,9 +59,10 @@ function RenderLoadingMessage(props){
 }
 
 function RenderError(props){
+  const {classes} = props
   if (props.message !== ""){
     return (
-      <Typography variant="subheading" style={{color: 'red'}} gutterBottom>
+      <Typography variant="subheading" style={{color: 'red'}} gutterBottom className={classes.messageContainer} align="center">
         {props.message}
       </Typography>
     )
@@ -53,7 +76,8 @@ export class Descobrir extends Component {
     super(props)
 
     this.state = {
-      videoURL:"https://www.youtube.com/watch?v=E-WHW-QNswE",
+      // videoURL:"https://www.youtube.com/watch?v=E-WHW-QNswE",
+      videoURL:"",
       currentTab: 0,
       gettingYoutubeInfo: false,
       sendingInfoToDatabase: false,
@@ -72,7 +96,11 @@ export class Descobrir extends Component {
   };
 
   handleClick(){
-    this.setState({
+    if (this.state.videoURL === ""){
+      this.setState({errorMessage: "You need to enter a URL to proceed "})
+      return
+    }
+    this.setState({ 
       gettingYoutubeInfo: true,
       sendingInfoToDatabase: false,
       currentStatusInfo: "Getting information from YouTube API...",
@@ -123,30 +151,37 @@ export class Descobrir extends Component {
       <div className={classes.root}>
         <Grid container spacing={24} direction="row" justify="center" alignItems="center">
           <Grid item xs={12}>
-            <Tabs value={currentTab} onChange={this.handleTabChange}>
-              <Tab label="Descobrir" />
-              <Tab label="Verificar" />
+            <Tabs value={currentTab} onChange={this.handleTabChange} fullWidth>
+              <Tab label="Discovery" />
+              <Tab label="Verify" />
             </Tabs>
           </Grid>
           {currentTab === 0 ? (
-            <div>
-              <Grid item xs={12}>
-                <TextField
-                  id="videoURL"
-                  label="URL do vídeo"
-                  value={this.state.videoURL}
-                  onChange={this.handleChange('videoURL')}
-                  margin="normal"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Button variant="contained" color="primary" onClick={() => {this.handleClick()}}>
-                  Descubra
-                </Button>
-                <RenderLoadingMessage message={currentStatusInfo}/>
-                <RenderError message={errorMessage}/>
-              </Grid>
-            </div>
+            <Grid container className={classes.discoveryContainer} direction="row" justify="center" alignItems="center">
+              <Paper elevation={24} className={classes.paperRoot}>
+                <Typography variant="title" gutterBottom>
+                  Enter a video URL to discover your category
+                </Typography>
+                <Grid item xs={12}>
+                  <TextField
+                    id="videoURL"
+                    label="Video URL"
+                    value={this.state.videoURL}
+                    onChange={this.handleChange('videoURL')}
+                    margin="normal"
+                    className={classes.videoURLTF}
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button variant="contained" color="primary" onClick={() => {this.handleClick()}} fullWidth>
+                    Discovery
+                  </Button>
+                  <RenderLoadingMessage message={currentStatusInfo} classes={classes}/>
+                  <RenderError message={errorMessage} classes={classes}/>
+                </Grid>
+              </Paper>
+            </Grid>
           ) : null}
           {currentTab === 1 ? (
             <div>
